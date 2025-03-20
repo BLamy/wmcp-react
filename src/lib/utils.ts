16376d487c7f47ad79ce30e45b6b6446bdc68bd1
Brett Lamy <brettlamy@gmail.com>
2025-03-20 03:23:58 -0400

    wip

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,17 @@
 import { bufferToBase64URLString, base64URLStringToBuffer } from '../webauthn/utils';
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+/**
+ * Combines multiple class names using clsx and merges Tailwind CSS classes with tailwind-merge
+ * to handle conflicts and duplications properly.
+ * 
+ * @param inputs - Class values to be combined (strings, objects, arrays, etc.)
+ * @returns A string of merged class names
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export { bufferToBase64URLString, base64URLStringToBuffer };
 
