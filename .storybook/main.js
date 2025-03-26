d2c709a64e231c523fd5ea4c896fa695528c2760
Brett Lamy <brettlamy@gmail.com>
2025-03-26 00:25:05 -0400

    wip

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,6 +10,7 @@ const config = {
     "../stories/**/MCPToolRouter.stories.@(js|jsx|mjs|ts|tsx)",
     "../stories/**/MCPToolRouterChat.stories.@(js|jsx|mjs|ts|tsx)",
     "../stories/**/SecureNotesDemo.stories.@(js|jsx|mjs|ts|tsx)",
+    "../stories/**/SecureNotesApp.stories.@(js|jsx|mjs|ts|tsx)",
     "../stories/**/SemanticToolChat.stories.@(js|jsx|mjs|ts|tsx)",
     "../stories/**/ChatList.stories.@(js|jsx|mjs|ts|tsx)",
     "../src/pglite/**/*.stories.@(js|jsx|mjs|ts|tsx)",
